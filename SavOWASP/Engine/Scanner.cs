49ae34b6c73b49c30ce5bf189a2e51c4f2457da5
Tomas Damonte <tomas.damonte@comunidad.ub.edu.ar>
2022-04-09 20:03:07 -0300

    cambiado a api

---
+++ SavOWASP/Engine/Scanner.cs
@@ -0,0 +1,210 @@
+﻿using Newtonsoft.Json;
+using OWASPZAPDotNetAPI;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace SavOWASP
+{
+    public class Scanner
+    {
+        private string _target;
+        private string _apikey = "lsi776rff5176e6eb3a6ts8n39";
+        private ClientApi _api;
+        private IApiResponse _apiResponse;
+
+        public Scanner(string target)
+        {
+            _api = new ClientApi("localhost", 8080, _apikey);
+            _target = target;
+            _api.alert.deleteAllAlerts();
+            ApiResponseList sites = (ApiResponseList)_api.core.sites();
+            foreach(ApiResponseElement site in sites.List)
+            {
+                _api.core.deleteSiteNode(site.Value,"","");
+            }           
+        }
+
+        public void Go()
+        {
+            string spiderScanId = StartSpidering();
+            PollTheSpiderTillCompletion(spiderScanId);
+
+            //StartAjaxSpidering();
+            //PollTheAjaxSpiderTillCompletion();
+
+            //string activeScanId = StartActiveScanning();
+            //PollTheActiveScannerTillCompletion(activeScanId);
+
+            string reportFileName = $"Report_{DateTime.Now:yyy-MM-dd-hh-mm-ss}.html";
+            //WriteXmlReport(reportFileName);
+            WriteHtmlReport(reportFileName);
+            Process.Start(reportFileName);
+            //PrintAlertsToConsole();
+
+            //ShutdownZAP();
+        }
+
+        public void Scan()
+        {
+            PollTheSpiderTillCompletion(StartSpidering());
+            PollTheActiveScannerTillCompletion(StartActiveScanning());
+        }
+
+        public string HtmlResult
+        {
+            get
+            {
+                return Encoding.UTF8.GetString(_api.core.htmlreport());
+            }
+        }
+
+        public string JsonResult
+        {
+            get
+            {
+                return Encoding.UTF8.GetString(_api.core.jsonreport());
+            }
+        }
+
+        public string XmlResult
+        {
+            get
+            {
+                return Encoding.UTF8.GetString(_api.core.xmlreport());
+            }
+        }
+
+        public OWASPZAPReport Result
+        {
+            get
+            {
+                return JsonConvert.DeserializeObject<OWASPZAPReport>(JsonResult);
+            }
+        }
+
+        private void ShutdownZAP()
+        {
+            _apiResponse = _api.core.shutdown();
+            if ("OK" == ((ApiResponseElement)_apiResponse).Value)
+                Console.WriteLine("ZAP shutdown success " + _target);
+        }
+
+        private void PrintAlertsToConsole()
+        {
+            //no anda
+            //List<Alert> alerts = _api.GetAlerts(_target, 0, 0, "");
+            //foreach (Alert alert in alerts)
+            //{
+            //    Console.WriteLine(alert.AlertMessage
+            //        + Environment.NewLine
+            //        + alert.CWEId
+            //        + Environment.NewLine
+            //        + alert.Url
+            //        + Environment.NewLine
+            //        + alert.WASCId
+            //        + Environment.NewLine
+            //        + alert.Evidence
+            //        + Environment.NewLine
+            //        + alert.Parameter
+            //        + Environment.NewLine
+            //    );
+            //}
+        }
+
+        private void WriteHtmlReport(string reportFileName)
+        {
+            File.WriteAllBytes(reportFileName, _api.core.htmlreport());
+        }
+
+        private void WriteXmlReport(string reportFileName)
+        {
+            File.WriteAllBytes(reportFileName + ".xml", _api.core.xmlreport());
+        }
+
+        private void PollTheActiveScannerTillCompletion(string activeScanId)
+        {
+            int activeScannerprogress;
+            while (true)
+            {
+                //Sleep(1000);
+                activeScannerprogress = int.Parse(((ApiResponseElement)_api.ascan.status(activeScanId)).Value);
+                //Console.WriteLine("Active scanner progress: {0}%", activeScannerprogress);
+                if (activeScannerprogress >= 100)
+                    break;
+            }
+            //Console.WriteLine("Active scanner complete");
+        }
+
+        private string StartActiveScanning()
+        {
+            //Console.WriteLine("Active Scanner: " + _target);
+            _apiResponse = _api.ascan.scan(_target, "", "", "", "", "", "");
+            return ((ApiResponseElement)_apiResponse).Value;
+        }
+
+        private void PollTheAjaxSpiderTillCompletion()
+        {
+            while (true)
+            {
+                Sleep(1000);
+                string ajaxSpiderStatusText = string.Empty;
+                ajaxSpiderStatusText = Convert.ToString(((ApiResponseElement)_api.ajaxspider.status()).Value);
+                if (ajaxSpiderStatusText.IndexOf("running", StringComparison.InvariantCultureIgnoreCase) > -1)
+                    Console.WriteLine("Ajax Spider running");
+                else
+                    break;
+            }
+            Console.WriteLine("Ajax Spider complete");
+            Sleep(10000);
+        }
+
+        private void StartAjaxSpidering()
+        {
+            Console.WriteLine("Ajax Spider: " + _target);
+            _apiResponse = _api.ajaxspider.scan(_target, "", "", "");
+
+            if ("OK" == ((ApiResponseElement)_apiResponse).Value)
+                Console.WriteLine("Ajax Spider started for " + _target);
+        }
+
+        private void PollTheSpiderTillCompletion(string scanid)
+        {
+            int spiderProgress;
+            while (true)
+            {
+                //Sleep(1000);
+                spiderProgress = int.Parse(((ApiResponseElement)_api.spider.status(scanid)).Value);
+                //Console.WriteLine("Spider progress: {0}%", spiderProgress);
+                if (spiderProgress >= 100)
+                    break;
+            }
+            //Console.WriteLine("Spider complete");
+        }
+
+        private string StartSpidering()
+        {
+            //Console.WriteLine("Spider: " + _target);
+            _apiResponse = _api.spider.scan(_target, "", "", "", "");
+            return ((ApiResponseElement)_apiResponse).Value;
+        }
+
+        private void LoadTargetUrlToSitesTree()
+        {
+            _api.AccessUrl(_target);
+        }
+
+        private static void Sleep(int milliseconds)
+        {
+            do
+            {
+                Thread.Sleep(milliseconds);
+                //Console.WriteLine("...zz" + Environment.NewLine);
+                milliseconds -= 2000;
+            } while (milliseconds > 2000);
+        }
+
+    }
+}
---
+++ SavOWASP/Models/OWASPZAPReport.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SavOWASP
+{
+    public class OWASPZAPReport
+    {
+        [JsonProperty("@version")]
+        public string Version { get; set; }
+
+        [JsonProperty("@generated")]
+        public string Generated { get; set; }
+        public List<Site> site { get; set; }
+    }
+
+    public class Instance
+    {
+        public string uri { get; set; }
+        public string method { get; set; }
+        public string param { get; set; }
+        public string attack { get; set; }
+        public string evidence { get; set; }
+    }
+
+    public class Alert
+    {
+        public string pluginid { get; set; }
+        public string alertRef { get; set; }
+        public string alert { get; set; }
+        public string name { get; set; }
+        public string riskcode { get; set; }
+        public string confidence { get; set; }
+        public string riskdesc { get; set; }
+        public string desc { get; set; }
+        public List<Instance> instances { get; set; }
+        public string count { get; set; }
+        public string solution { get; set; }
+        public string otherinfo { get; set; }
+        public string reference { get; set; }
+        public string cweid { get; set; }
+        public string wascid { get; set; }
+        public string sourceid { get; set; }
+    }
+
+    public class Site
+    {
+        [JsonProperty("@name")]
+        public string Name { get; set; }
+
+        [JsonProperty("@host")]
+        public string Host { get; set; }
+
+        [JsonProperty("@port")]
+        public string Port { get; set; }
+
+        [JsonProperty("@ssl")]
+        public string Ssl { get; set; }
+        public List<Alert> alerts { get; set; }
+    }
+
+}
