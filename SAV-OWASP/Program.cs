9b39850bd4fd3882d3b406086ef0347967f5f3ee
Tomas Damonte <tomas.damonte@comunidad.ub.edu.ar>
2022-06-09 15:20:29 -0300

    agregado unit test

--- SAV-OWASP/Program.cs
+++ SAV-OWASP/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Owin.Hosting;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
 
 namespace SavOWASP
 {
@@ -8,10 +11,22 @@ namespace SavOWASP
     {
         static void Main(string[] args)
         {
+            while (!IsZAPOWASPRunning())
+            {
+                Console.WriteLine("OWASP ZAP no se esta ejecutando. Por favor ejecute OWASP ZAP para continuar.");
+                Console.WriteLine("Presione Enter para continuar...");
+                Console.ReadLine();
+            }            
             string baseAddress = ConfigurationManager.AppSettings["BaseAdress"];
             WebApp.Start<Startup>(baseAddress);
             Console.WriteLine($"Servicio SAV-OWASP ejecutandose en {baseAddress}{Environment.NewLine}Presione una tecla para salir.");
             Console.ReadKey();             
+        }
+
+        private static bool IsZAPOWASPRunning()
+        {
+            var procesos = new List<Process>(Process.GetProcesses());
+            return procesos.Any(p => p.MainWindowTitle.ToLower().Contains("owasp zap")) ;
         }
 
     }
---
+++ UnitTests/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavOWASP;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private ScannerController Controller = new ScannerController();
+        private string TestURL = "https://conectate.ub.edu.ar";
+
+        [TestMethod]
+        public void TestScan()
+        {
+            var result = Controller.Scan(TestURL);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestScanToHtml()
+        {
+            var result = Controller.ScanToHtml(TestURL);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.IsSuccessStatusCode);            
+        }
+
+    }
+}
